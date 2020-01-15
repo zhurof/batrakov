456bb997c82e29525c2c455f6e756b8bb347ece1
zhurof <41473707+zhurof@users.noreply.github.com>
2020-01-15 16:13:42 +0300

    homework

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@
 		$(this).siblings('ul').slideToggle();
 	}
 })
+$('.menu-btn').click(function(){
+	$(this).toggleClass('menu-btn--active');
+	$('.header__menu').toggleClass('header__menu--open');
+})
+$('.header__menu a').click(function(e){
+	if($(this).siblings('ul').length && innerWidth < 1030){
+		e.preventDefault();
+		$(this).parent('li').toggleClass('open');
+	}
+})
---
+++ tools.php
@@ -0,0 +1,55 @@
+﻿<?php	include "header.php";?>		
+	<main class="sidebar-page">
+		<div class="wrapper sidebar-page__inner">
+			<div class="sidebar-page__content">
+				<div class="breadcrumbs">
+					<a href="/">Главная</a>
+					&nbsp;&nbsp;–&nbsp;&nbsp;
+					<span>Инструменты</span>
+				</div>
+				<article class="text-block">
+					<h1>Инструменты</h1>
+					<h3>Контроль</h3>
+					<ul>
+						<li><a href="https://sites.pr-cy.ru/" target="_blank">https://sites.pr-cy.ru/</a></li>
+						<li><a href="https://webmaster.yandex.ru/" target="_blank">https://webmaster.yandex.ru/</a></li>
+						<li><a href="https://www.google.com/webmasters/" target="_blank">https://www.google.com/webmasters/</a></li>
+					</ul>
+					<h3>Статистика</h3>
+					<ul>
+						<li><a href="https://metrika.yandex.ru/" target="_blank">https://metrika.yandex.ru/</a></li>
+						<li><a href="https://analytics.google.com/" target="_blank">https://analytics.google.com/</a></li>
+					</ul>
+					<h3>Работа с семантикой</h3>
+					<ul>
+						<li><a href="https://wordstat.yandex.ru/" target="_blank">https://wordstat.yandex.ru/</a></li>
+						<li><a href="https://ads.google.com/intl/ru_ru/home/tools/keyword-planner/" target="_blank">https://ads.google.com/intl/ru_ru/home/tools/keyword-planner/</a></li>
+					</ul>
+					<h3>Анализ ссылочного профиля</h3>
+					<ul>
+						<li><a href="https://ahrefs.com/" target="_blank">https://ahrefs.com/</a></li>
+						<li><a href="https://ru.majestic.com/" target="_blank">https://ru.majestic.com/</a></li>
+						<li><a href="https://linkchecker.pro/ru/" target="_blank">https://linkchecker.pro/ru/</a></li>
+					</ul>
+					<h3>Контроль</h3>
+					<ul>
+						<li><a href="https://sites.pr-cy.ru/" target="_blank">https://sites.pr-cy.ru/</a></li>
+						<li><a href="https://webmaster.yandex.ru/" target="_blank">https://webmaster.yandex.ru/</a></li>
+						<li><a href="https://www.google.com/webmasters/" target="_blank">https://www.google.com/webmasters/</a></li>
+					</ul>
+					<h3>Статистика</h3>
+					<ul>
+						<li><a href="https://metrika.yandex.ru/" target="_blank">https://metrika.yandex.ru/</a></li>
+						<li><a href="https://analytics.google.com/" target="_blank">https://analytics.google.com/</a></li>
+					</ul>
+					<h3>Работа с семантикой</h3>
+					<ul>
+						<li><a href="https://wordstat.yandex.ru/" target="_blank">https://wordstat.yandex.ru/</a></li>
+						<li><a href="https://ads.google.com/intl/ru_ru/home/tools/keyword-planner/" target="_blank">https://ads.google.com/intl/ru_ru/home/tools/keyword-planner/</a></li>
+					</ul>
+				</article>				
+			</div>
+			<? include "aside.php";?>
+		</div>		
+	</main>
+<?php	include "footer.php";?>	
